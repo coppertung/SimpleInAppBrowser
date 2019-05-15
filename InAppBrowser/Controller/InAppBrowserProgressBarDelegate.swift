cf2b2e4bc37e0c1edb3218c7ca9ae32aff7386b3
Copper <siutungwu@gmail.com>
2019-05-15 15:23:15 +0800

    add comment

--- InAppBrowser/Controller/InAppBrowserProgressBarDelegate.swift
+++ InAppBrowser/Controller/InAppBrowserProgressBarDelegate.swift
@@ -2,6 +2,15 @@ import Foundation
 
 protocol InAppBrowserProgressBarDelegate {
     
+    /**
+     *
+     * Callback function that will be invoked when the progress of loading webview has been updated.
+     * This will be an empty function by default.
+     *
+     * - Parameter browser: Current InAppBrowserView.
+     * - Parameter progress: Current progress. (0 ~ 1)
+     *
+    */
     func onLoadProgress(_ browser: InAppBrowserView, progress: CGFloat)
     
 }
